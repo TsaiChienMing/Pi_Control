214d9973fb20cb63858fd303aa86de330402641e
weiting <treasure40080@gmail.com>
2020-11-14 14:04:27 +0800

    pi_control

---
+++ app/src/main/java/tw/tim/pi_control/MainActivity.java
@@ -0,0 +1,79 @@
+package tw.tim.pi_control;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private Socket socket;
+
+    private static final int SERVERPORT = 5000;
+    private static final String SERVER_IP = "192.168.1.104";
+    private Button connect;
+    private EditText et;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupViewComponent();
+
+        new Thread(new ClientThread()).start();
+    }
+
+    private void setupViewComponent(){
+        et = (EditText) findViewById(R.id.et_input);
+        connect = (Button)findViewById(R.id.btn_connect);
+        connect.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_connect:
+                try {
+                    String str = et.getText().toString();
+                    PrintWriter out = new PrintWriter(new BufferedWriter(
+                            new OutputStreamWriter(socket.getOutputStream())),
+                            true);
+                    out.println(str);
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+        }
+    }
+
+
+    class ClientThread implements Runnable {
+        @Override
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+                socket = new Socket(serverAddr, SERVERPORT);
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+}
